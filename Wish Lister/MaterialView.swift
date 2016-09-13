cac5e2028c7cee93c51d958cbf7e3d8f32c8bdb0
Derek Conway <DRKCONWAY@GMAIL.COM>
2016-09-13 16:00:43 -0500

    major changes including adding style to the view

--- Wish Lister/MaterialView.swift
+++ Wish Lister/MaterialView.swift
@@ -26,6 +26,25 @@ extension UIView {
         set {
             
             materialKey = newValue
+            
+            if materialKey {
+                
+                
+                self.layer.masksToBounds = false
+                self.layer.cornerRadius = 9.0
+                self.layer.shadowOpacity = 0.8
+                self.layer.shadowRadius = 4.0
+                self.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+                self.layer.shadowColor = UIColor(red: 157/255, green: 157/255, blue: 157/255, alpha: 1.0).cgColor
+                
+            } else {
+                
+                self.layer.cornerRadius = 0
+                self.layer.shadowOpacity = 0
+                self.layer.shadowRadius = 0
+                self.layer.shadowColor = nil
+                
+            }
         }
         
     }
