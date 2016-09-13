69b478d7d1b253aacb55d05055a4f91443e19dcd
Derek Conway <DRKCONWAY@GMAIL.COM>
2016-09-13 16:25:53 -0500

    setup UI connections

---
+++ Wish Lister/ItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemCell.swift
+//  Wish Lister
+//
+//  Created by D on 9/13/16.
+//  Copyright © 2016 D Conway. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var Thumbnail: UIImageView!
+    @IBOutlet weak var Title: UILabel!
+    @IBOutlet weak var Price: UILabel!
+    @IBOutlet weak var Details: UILabel!
+    
+}
--- Wish Lister/MainViewController.swift
+++ Wish Lister/MainViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
     }
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 0
+    }
 
 }
 
